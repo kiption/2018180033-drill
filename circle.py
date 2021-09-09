df10cedb7e9a6ddc86664c9111e77c9fd8dcdbb8
kiption <seichulee@naver.com>
2021-09-09 20:31:56 +0900

    이름 만들기 과제 추가.

---
+++ circle.py
@@ -0,0 +1,10 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import turtle
+>>> 
+>>> turtle.penup()
+>>> turtle.goto(200,200)
+>>> turtle.pendown()
+>>> turtle.circle(50)
+>>> turtle.write("200,200")
+>>> turtle.exitonclick()
