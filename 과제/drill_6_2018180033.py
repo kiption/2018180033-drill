6a94237af1d58ec397ac98a864dc0651b5af6d83
kiption <seichulee@naver.com>
2021-09-16 18:48:24 +0900

    drill_06 2018180033_이세철_사각형_원형_구현완료.

--- 과제/drill_6_2018180033.py
+++ 과제/drill_6_2018180033.py
@@ -6,8 +6,8 @@ grass = load_image('grass.png')
 character = load_image('character.png')
 x = 400
 y = 90
-sinx = 0
-cosx = 0
+theta=270
+
 change_mode = 0
 count=0;
 while True:
@@ -21,7 +21,9 @@ while True:
                 delay(0.01)
                 if x>=800:
                     count = 1                    
-
+                elif x == 400:
+                    change_mode = 1
+                    break
         elif count==1:
             while y < 600:
                 clear_canvas_now()
@@ -48,5 +50,18 @@ while True:
                 y-=2
                 delay(0.01)
                 if  y <= 90:
-                    count=0            
+                    count=0
+                    
+    elif change_mode == 1:
+        y=210*math.sin(theta/360*2*math.pi)+300
+        x=210*math.cos(theta/360*2*math.pi)+400
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,y)
+        theta-=1
+        delay(0.01)
+        if theta == -90:
+            change_mode = 0
+            theta = 270
+
 close_canvas()
