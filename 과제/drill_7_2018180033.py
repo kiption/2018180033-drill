b15bafafd6223467df5c9beaceaecde4a5aa3d39
kiption <seichulee@naver.com>
2021-10-06 16:28:54 +0900

    drill #07 완성_2018180033
    유튜브 링크 https://www.youtube.com/watch?v=PPNcz29cQdE

--- 과제/drill_7_2018180033.py
+++ 과제/drill_7_2018180033.py
@@ -9,7 +9,7 @@ def handle_events():
     global mouse_x, mouse_y
     events = get_events()
     for events in events:
-        if events.type ==SDL_QUIT:
+        if events.type == SDL_QUIT:
             running = False
         elif events.type == SDL_MOUSEMOTION:
             mouse_x, mouse_y = events.x, 600 - 1 - events.y
@@ -23,8 +23,14 @@ def handle_events():
             elif events.key == SDLK_ESCAPE:
                 running= False;
 
+def character_move():
+    global x, y
+    global mouse_x, mouse_y
 
-    pass
+    x = (1-0.05)*x+0.05*mouse_x
+    y = (1-0.05)*y+0.05*mouse_y
+
+
 
 
 open_canvas()
@@ -34,10 +40,9 @@ cursor = load_image('hand_arrow.png')
 hide_cursor()
 running = True
 
-running = True
 frame = 0
-dir = 0 # -1 left,+1 right
-ani = 3 # 1오른쪽 0왼쪽
+dir = 0
+ani = 3
 x, y = 800 // 2, 600 // 2
 mouse_x = 500
 mouse_y = 600//2
@@ -45,17 +50,13 @@ hide_cursor()
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * ani, 100, 100, x, 90)
+    character.clip_draw(frame * 100, 100 * ani, 100, 100, x, y)
     cursor.clip_draw(0, 0, 50, 52, mouse_x, mouse_y)
-
     update_canvas()
-
+    character_move()
     handle_events()
     frame = (frame + 1) % 8
-    if(x<mouse_x):
-        x += dir*5
-    elif (x >= mouse_x):
-        x -= dir * 5
+
     delay(0.01)
 
 close_canvas()
